1ef1823f1c7b806d0b98a91b0483385825f59eff
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-07-29 15:55:56 +0900

    use databinding
    show livedoor news

--- app/src/main/java/com/kaz/news_picker/MainActivity.kt
+++ app/src/main/java/com/kaz/news_picker/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.kaz.news_picker
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
 import android.util.Log
-import android.widget.TextView
+import com.kaz.news_picker.databinding.ActivityMainBinding
 import com.squareup.okhttp.ResponseBody
-import retrofit.Response
+import org.jsoup.Jsoup
 import rx.Subscriber
 import rx.Subscription
 import rx.android.schedulers.AndroidSchedulers
@@ -17,7 +19,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
         Log.d("onCreate","onCreate")
 
@@ -26,10 +29,15 @@ class MainActivity : AppCompatActivity() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(object : Subscriber<ResponseBody>() {
                     override fun onNext(t: ResponseBody?) {
-
                         Log.d("onCreate","onNext")
-                        val textView = findViewById(R.id.textView) as TextView
-                        textView.text = "${t?.string()}"
+                        val document = Jsoup.parse(t?.string())
+//                        val title = document.title()
+                        //val textView =  //findViewById(R.id.textView) as TextView
+                        val texts: List<String> = document.select("#main > div > section > div > div > div > ul > li > a")
+                                .map { it.text() }
+                        val a = TextUtils.join("\n", texts)
+
+                        binding.textView.text = "${a}"
                     }
 
                     override fun onError(e: Throwable?) {
